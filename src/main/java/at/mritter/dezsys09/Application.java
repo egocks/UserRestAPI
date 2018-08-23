4dfb14e157eadbf2f3b7c1a6db994375c39f571e
elielg <eliel.goco@pointwest.com.ph>
2018-08-23 19:25:38 +0800

    sample change again

--- src/main/java/at/mritter/dezsys09/Application.java
+++ src/main/java/at/mritter/dezsys09/Application.java
@@ -19,7 +19,7 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Test change");
+        System.out.println("Test change 2");
     }
 }
 
